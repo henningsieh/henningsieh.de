06427ad091302d3a296c56365bbcb2948f823a2c
Henning Sieh <admin@henningsieh.de>
2026-06-06 05:26:45 +0200

    chore: remove unoptimized image setting from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   // output: "export", // Uncomment this line if you want to export a static site
-  images: { unoptimized: true },
 }
 
 export default nextConfig
