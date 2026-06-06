7d0adf44b7112d835ed15d9730ae7dd1431c5bfb
Henning Sieh <admin@henningsieh.de>
2026-06-06 05:41:55 +0200

    chore: ensure reactStrictMode is enabled in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   // output: "export", // Uncomment this line if you want to export a static site
 }
 
