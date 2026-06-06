1e09f52b121c1aae4d9751f51929d8591b844a38
Henning Sieh <admin@henningsieh.de>
2026-06-06 05:43:00 +0200

    chore: enable static site export in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // output: "export", // Uncomment this line if you want to export a static site
+  output: "export",
 }
 
 export default nextConfig
